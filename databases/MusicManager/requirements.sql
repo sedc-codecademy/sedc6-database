f950bad4016fa4a4bcceabd4b59357eacdb0def8
xhevat <xhevat.i@outlook.com>
2018-04-21 13:27:01 +0200

    added mm.sql requirements

---
+++ databases/MusicManager/requirements.sql
@@ -0,0 +1,17 @@
+-- how many Songs start with the letter 'a' (case insensitive)
+-- how many artists end with letter 'a' (case insensitive)
+-- whats the name of the song with longest duration
+-- whats the total Duration of all Songs
+-- how many albums have Songs longer than 300 seconds
+-- print the names of the artists(separated with "--"), that have more than one album of PopRock genre
+-- print the name of the album that has highest Average duration of a song
+-- how many characters has the song that has the shortest Duration
+-- print the name and the genre of the album that has most songs
+-- print the name of the artist that has most songs
+-- print the type of the artist(SoloArtist/Band) that has most albums published before year 2000
+-- print the average song duration, of the album that has most songs
+
+
+-- print the longest song duration of the album that has least songs
+-- print the name of the album that has most songs that contain letter 'a' in the name
+-- print the name of the artist that has most songs that end wit letter 'd'
